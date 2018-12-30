566ef5d51980040494566017d445382af0f46b90
SIMON <szytyl@gmail.com>
2018-12-30 13:12:46 +0100

    dodanie kulek

--- app/src/main/java/com/example/szymon/kulka/MainActivity.java
+++ app/src/main/java/com/example/szymon/kulka/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.szymon.kulka;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
@@ -8,6 +9,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,91 +20,79 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener2 {
 
-    private FrameLayout gameFrame;
     private LinearLayout startLayout;
-
-
-    private ImageView red, blue;
-
-    //private int BoxSize;
-
+    private ImageView red, blue, white, green, orange, purple, yellow;
     private float redX, redY;
+    private float whiteX, whiteY;
+    private float greenX, greenY;
+    private float orangeX, orangeY;
     private float blueX, blueY;
-    private float xMax, yMax;
+    private float purpleX, purpleY;
+    private float yellowX, yellowY;
+    private int xMax, yMax;
     private float xChange, yChange;
-    private float xPos, xVel, yPos, yVel;
-
     private TextView scoreLabel, highScoreLabel;
     private int score, highScore, timeCount;
-
+    private SharedPreferences settings;
     private SensorManager sensorManager;
-
-
     private Timer timer;
-    private Handler handler = new Handler();
-
-    private boolean start_flg = false;
-    private boolean action_flg = false;
-   // private boolean pink_flg = false;
-
-
-    //sensor
+    private float xRand = 3;
+    private float yRand = 3;
 
     public void startGame(View view) {
-        start_flg = true;
         startLayout.setVisibility(View.INVISIBLE);
+        white.setX(xMax/2);
+        white.setY(yMax/2);
+        whiteX = red.getX();
+        whiteY = red.getY();
 
-        red.setX(0);
-        red.setY(0);
-        //blue.setX(10.0f);
-
-        redX = red.getX();
-        redY = red.getY();
-
-        //blueX = blue.getX();
+        blueX = blue.getX();
+        blueY = blue.getY();
 
         red.setVisibility(View.VISIBLE);
-        blue.setVisibility(View.INVISIBLE);
-
+        blue.setVisibility(View.VISIBLE);
 
         timeCount = 0;
         score = 0;
         scoreLabel.setText("Wynik = 0");
 
-        timer = new Timer();
+        settings = getSharedPreferences("GAME_DATA", Context.MODE_PRIVATE);
+        highScore = settings.getInt("HIGH_SCORE", 0);
+        highScoreLabel.setText("Rekord = " + highScore);
+
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-
-
-        gameFrame = findViewById(R.id.gameFrame);
         startLayout = findViewById(R.id.startLayout);
         red = findViewById(R.id.red);
+        white = findViewById(R.id.white);
+        green = findViewById(R.id.green);
+        yellow = findViewById(R.id.yellow);
+        orange = findViewById(R.id.orange);
+        purple = findViewById(R.id.purple);
         blue = findViewById(R.id.blue);
         scoreLabel = findViewById(R.id.scoreLabel);
         highScoreLabel = findViewById(R.id.highScoreLabel);
 
-        //max wymiary
-        Point size = new Point();
+        Point size = new Point();                                                                   //max wymiary
         Display display = getWindowManager().getDefaultDisplay();
         display.getSize(size);
-        xMax = (float) size.x - 97;
-        yMax = (float) size.y - 50;
+        xMax = size.x - 97;
+        yMax = size.y - 50;
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -119,10 +109,68 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            xChange = sensorEvent.values[0];
-            yChange = -sensorEvent.values[1];
-            updateBall();
+            xChange = (sensorEvent.values[0])*2;
+            yChange = (-sensorEvent.values[1])*2;
+            updateWhiteBall();
+            ballMovement();
         }
+    }
+
+    public void updateWhiteBall() {
+        whiteX -= xChange;
+        whiteY -= yChange;
+
+        if (whiteX > xMax) {
+            whiteX = xMax;
+        } else if (whiteX < -15) {
+            whiteX = -15;
+        }
+        if (whiteY > yMax) {
+            whiteY = yMax;
+        } else if (whiteY < 112) {
+            whiteY = 112;
+        }
+        white.setX(whiteX);
+        white.setY(whiteY);
+    }
+
+    public void ballMovement(){
+
+
+        do {
+
+            if (blueX > xMax) {
+                xRand = -3;
+            } else if (blueX < -15) {
+                xRand = 3;
+            }
+            if (blueY > yMax) {
+               yRand= -3;
+            } else if (blueY < 112) {
+                yRand = 3;
+            }
+            blueX += xRand;
+            blueY += yRand;
+            blue.setX(blueX);
+            blue.setY(blueY);
+        }while(hitCheck(whiteX,whiteY,blueX,blueY));
+
+    }
+
+    public boolean hitCheck(float xMain, float yMain, float xCpu, float yCpu){
+       float  r=70;
+        if(java.lang.Math.abs(xMain - xCpu) < r && java.lang.Math.abs(yMain- yCpu) <r){
+            score += 1;
+            scoreLabel.setText("Wynik = "+ score);
+            Random rand = new Random();
+            blueX = rand.nextInt(xMax);
+            blueY = rand.nextInt(yMax);
+
+           // blueX = blue.getX();
+           // blueY = blue.getY();
+            return true;
+            }
+            return false;
     }
 
     @Override
@@ -134,68 +182,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private void updateBall() {
-        redX -= xChange;
-        redY -= yChange;
-
-        if (redX > xMax) {
-            redX = xMax;
-        } else if (redX < -15) {
-            redX = -15;
+    public void quitGame (View view){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAndRemoveTask();
+        } else {
+            finish();
         }
-
-        if (redY > yMax) {
-            redY = yMax;
-        } else if (redY < 112) {
-            redY = 112;
-        }
-
-
-        red.setX(redX);
-        red.setY(redY);
-
-
-
-        /* float frameTime = 0.666f;
-        xVel += (redX * frameTime);
-        yVel += (redY * frameTime);
-
-        float xS = (xVel / 2) * frameTime;
-        float yS = (yVel / 2) * frameTime;
-
-        redX -= xS;
-        redY -= yS;
-*/
     }
-
-    public boolean hitCheck(){
-        
-    }
-
-
-
-
-
-
-
-
-
 }
-     /*   timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if (start_flg){
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            updateBall();
-                        }
-                    });
-            }
-        }, 0, 20);}
-*/
- /*  public void quitGame (View view){
 
-    }
-};
-*/
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
